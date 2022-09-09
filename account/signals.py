efa0d8ebcf7e60e380fac22aea9ad3c82698fe13
xuzhao <twtyjvkg@foxmail.com>
2022-09-10 01:26:36 +0800

    [ADD] signals

--- account/signals.py
+++ account/signals.py
@@ -9,7 +9,16 @@ User = get_user_model()
 
 @receiver(signals.post_save, sender=User)
 def create_user(sender, instance=None, created=False, **kwargs):
+    """
+    创建用户信号量
+    :param sender:
+    :param instance: 实例
+    :param created: 是否新建
+    :param kwargs:
+    :return:
+    """
     if created:
+        """创建用户时将明文密码改成加密格式"""
         password = instance.password
         instance.set_password(password)
         instance.save()
